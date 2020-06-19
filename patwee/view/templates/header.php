d66c6e6dca2608a6910ea92089e9d8e9a2466997
anthony <99057187@mydavinci.nl>
2020-06-19 13:20:38 +0200

    no message

--- patwee/view/templates/header.php
+++ patwee/view/templates/header.php
@@ -3,22 +3,21 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>www.patwee.nl</title>	
+	<title>patwee.nl</title>	
 	<script src="https://kit.fontawesome.com/66499e4192.js" crossorigin="anonymous"></script>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 </head>
 <body>
 	<div id="container">
-		<nav class="navbar navbar-expand-lg navbar-dark" style="height:40px; background-color:#252525;">
-			<ul class="d-flex align-self-center">
-				<li  class="navbar-brand"><a href="<?= URL ?>home/index">Home</a></li>
-				<li  class="navbar-brand"><a href="<?= URL ?>snackbar/index">snackbar</a></li>
+		<nav class="navbar navbar-expand-lg navbar-dark justify-content-end" style="height:40px; background-color:#252525;">
+			<ul class="d-flex align-self-center ">
+				<li  class="navbar-brand"><a href="<?= URL ?>home/index">klantenservice</a></li>
 			</ul>
 		</nav>
 		<nav class="navbar navbar-expand-lg navbar-dark bg-warning sticky-top">
 			<ul class="d-flex align-self-center">
 				<img src="<?php echo URL ?>public/afbeeldingen/logo/patwee_logo.png?>" style="width:50px;">&nbsp;&nbsp;&nbsp;&nbsp;
-				<li  class="navbar-brand"><a href="<?= URL ?>home/index">Home</a></li>
+				<li  class="navbar-brand"><a href="<?= URL ?>home/index">films</a></li>
 				<li  class="navbar-brand"><a href="<?= URL ?>snackbar/index">snackbar</a></li>
 			</ul>
 		</nav>
