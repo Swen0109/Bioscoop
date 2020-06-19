73e5a970a93ad7baca4d2470bb03d30b31ef6d69
Swen <99058148@mydavinci.nl>
2020-06-19 14:19:32 +0200

    Commit
    Footer gemaakt

--- patwee/view/templates/footer.php
+++ patwee/view/templates/footer.php
@@ -1,11 +1,6 @@
 <div  class="py-3 bg-warning text-dark-50 fixed-bottom text-center">
-<<<<<<< HEAD
-<img class="card-img-top img-fluid" src="<?php echo URL ?>public/afbeeldingen/logo/patwee_logo.png">
-        &copy; Anthony Inocencio Ramos
-=======
 <img src="<?php echo URL ?>public/afbeeldingen/logo/patwee_logo_footer.png" style="width:99px;">&nbsp;&nbsp; 
          Anthony Inocencio Ramos & Swen Sperling
->>>>>>> master
     </div>
 </body>
 </html>
