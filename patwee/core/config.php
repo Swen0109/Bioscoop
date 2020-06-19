7252d0c58355768155cdec72a10a2c5fdbf16099
anthony <99057187@mydavinci.nl>
2020-06-19 12:17:59 +0200

    no message

--- patwee/core/config.php
+++ patwee/core/config.php
@@ -3,7 +3,7 @@
 // Database instellingen
 define('DB_TYPE', 'mysql');		// Wat voor type database gebruik je?
 define('DB_HOST', '127.0.0.1'); // Wat is het IP adres van de server (127.0.0.1 is de lokae machine)
-define('DB_NAME', 'framework'); // Wat is de database naam
+define('DB_NAME', 'patwee'); // Wat is de database naam
 define('DB_USER', 'root'); 		// Wat is de database gebruiker
 define('DB_PASS', 'mysql');			// Wat is het database wachtwoord
 define('DB_CHARSET', 'utf8'); 	// Welke karakterset wordt gebruikt
