b9b51bcf6dd12cc6c153ebaf3bd7c23f4c657586
anthony <99057187@mydavinci.nl>
2020-06-19 12:14:42 +0200

    no message

--- patwee/view/templates/footer.php
+++ patwee/view/templates/footer.php
@@ -1,6 +1,6 @@
 <div  class="py-3 bg-warning text-dark-50 fixed-bottom text-center">
-<img class="card-img-top img-fluid" src="<?php echo URL ?>public/afbeeldingen/logo/patwee_logo.png">
-        &copy; Anthony Inocencio Ramos
+<img class="card-img-top img-fluid" src="<?php echo URL ?>public/afbeeldingen/films/LineOfDuty.jpg">
+        &copy; Anthony Inocencio Ramos & Swen Sperling
     </div>
 </body>
 </html>
