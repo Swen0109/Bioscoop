a4d41b3a787f0c7cf65e569b8202b176cd13a585
Swen <99058148@mydavinci.nl>
2020-06-19 16:20:56 +0200

    Commit8
    Index pagina

--- patwee/view/home/index.php
+++ patwee/view/home/index.php
@@ -1,90 +1,19 @@
 <div class="container">
- <h1>Welkom bij het php-framework.</h1>
- <p>Dit is een test</p>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
- <br>
+ <h1 class="text-center">Welkom bij Patwee!</h1>
+<br>
+<br>
+<?php foreach ($films as $film){?>
+ 
+ <div style="width:200px; height:500px; "class="card float-left">
+  <img style="width:200px;" class="card-img-top" src="<?= URL ?>public/afbeeldingen/films/<?php echo $film['afbeelding']?>" alt="Card image cap">
+  <div class="card-body">
+    <h4 class="card-title"><?php echo $film['titel'];?></h4>
+    <a href="#!" class="btn btn-primary">Details</a>
+  </div>
+</div>
+  
+
+
+
+<?php } ?>
 </div>
