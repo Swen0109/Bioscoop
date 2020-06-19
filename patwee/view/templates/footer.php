0bcfce748e2a69d4c6867ca9e977b5c9f6fab1da
anthony <99057187@mydavinci.nl>
2020-06-19 14:10:47 +0200

    no message

--- patwee/view/templates/footer.php
+++ patwee/view/templates/footer.php
@@ -1,5 +1,5 @@
 <div  class="py-3 bg-warning text-dark-50 fixed-bottom text-center">
-<img src="<?php echo URL ?>public/afbeeldingen/logo/patwee_logo_footer.png" style="width:99px;">&nbsp;&nbsp;
+<img src="<?php echo URL ?>public/afbeeldingen/logo/patwee_logo_footer.png" style="width:99px;">&nbsp;&nbsp; 
          Anthony Inocencio Ramos & Swen Sperling
     </div>
 </body>
