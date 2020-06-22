b828776df5c6138af0e8b69cef2239e12a22a859
anthony <99057187@mydavinci.nl>
2020-06-22 15:32:33 +0200

    no message

--- patwee/snackbar (3).sql
+++ patwee/snackbar (3).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Gegenereerd op: 20 jun 2020 om 17:32
+-- Gegenereerd op: 22 jun 2020 om 13:31
 -- Serverversie: 8.0.18
 -- PHP-versie: 7.3.11
 
@@ -57,24 +57,24 @@ INSERT INTO `snackbar` (`id`, `naam`, `afbeelding`, `beschrijving`, `prijs`, `ty
 (13, 'mars', 'mars.png', 'mars niet de planeet maar de chocolade reep. Mars, geeft je energie', '0.99', 'snoep'),
 (14, 'snickers', 'snickers.png', 'you always lose your edge when you\'r hungry. snickers steelt stevige trek', '0.99', 'snoep'),
 (15, 'twix', 'twix.png', 'twix 2 verschillende chocolade repen in een verpakking.try both and pick a side.', '1.49', 'snoep'),
-(16, 'm&m\'s', 'm&m.png', '', '1.99', 'snoep'),
-(17, 'Skittles', 'skittles.png', '', '1.99', 'snoep'),
-(18, 'Maltesers', 'maltesers.png', '', '1.99', 'snoep'),
-(19, 'coca_cola', 'coca_cola.png', '', '1.69', 'drankje'),
-(20, 'coca cola zero', 'coca_cola_zero.png', '', '1.69', 'drankje'),
-(21, 'coca cola light', 'coca_cola_light.png', '', '1.69', 'drankje'),
-(22, 'Ice tea bubbles', 'Ice_tea_bubbles.png', '', '1.69', 'drankje'),
-(23, 'Ice tea no peach', 'Ice_tea_peach.png', '', '1.69', 'drankje'),
-(24, 'fanta', 'fanta.png', '', '1.69', 'drankje'),
-(25, 'capri sun', 'caprisun.png', '', '1.69', 'drankje'),
-(26, 'multi vicee', 'multi_vicee.png', '', '1.69', 'drankje'),
-(27, 'appelsap', 'appelsap.png', '', '1.69', 'drankje'),
-(28, 'fernadez cherry bouquet', 'fernadez_cherrybouquet.png', '', '1.39', 'drankje'),
-(29, 'fernandez green punch', 'fernandez_greenpunch.png', '', '1.39', 'drankje'),
-(30, 'fernandez super pineapple', 'fernandez_superpineapple.png', '', '1.39', 'drankje'),
-(31, 'kidz menu box 1', 'box1.png', '', '10.49', 'kidzMenu'),
-(32, 'kidz menu box 2', 'box2.png', '', '12.49', 'kidzMenu'),
-(33, 'kidz menu box 3', 'box3.png', '', '3.99', 'kidzMenu');
+(16, 'm&m\'s', 'm&m.png', 'M&m\'s chocolade omhulde noten. smelt in je mond niet in je hand.', '1.99', 'snoep'),
+(17, 'Skittles', 'skittles.png', 'skittle\'s kleur rijken snoepjes gevuld met smaak. taste the rainbow', '1.99', 'snoep'),
+(18, 'Maltesers', 'maltesers.png', 'maltesers een leuke fun bol chocolade.The lighter way to enjoy chocolate.', '1.99', 'snoep'),
+(19, 'coca_cola', 'coca_cola.png', 'coca cola een heerlijk verfrissend drankje. taste the feeling.', '1.69', 'drankje'),
+(20, 'coca cola zero', 'coca_cola_zero.png', 'coca cola nu ook met minder suiker. coca cola zero, taste the feeling.', '1.69', 'drankje'),
+(21, 'coca cola light', 'coca_cola_light.png', 'coca cola light met maar slechts 1 calorie de beste keuze voor mensen op dieet.taste the feeling', '1.69', 'drankje'),
+(22, 'Ice tea bubbles', 'Ice_tea_bubbles.png', 'lipton ice tea bubbles een heerlijke verfrissing gemaakt van natuurlijke producten en een beetje bubbles.', '1.69', 'drankje'),
+(23, 'Ice tea peach', 'Ice_tea_peach.png', 'lipton ice tea peach een heerlijke verfrissing gemaakt van natuurlijke groene thee blaadjes en perziken.', '1.69', 'drankje'),
+(24, 'fanta', 'fanta.png', 'fanta een heelrijke verfrissing gemaakt met verse sinaasappels. ontdek de FANTAstische smaak van fanta.', '1.69', 'drankje'),
+(25, 'capri sun', 'caprisun.png', 'capri sun volledig organisch, geen kunstmatig kleur of smaak stoffen toe gevoegd. maar toch de heerlijke smaak van sinaasappel.', '1.69', 'drankje'),
+(26, 'roos vicee', 'multi_vicee.png', 'roos vicee bom vol vitamine en smaak, schenk vertrouwen.\r\n', '1.69', 'drankje'),
+(27, 'appelsap', 'appelsap.png', 'appelsap vers gepersd en bordevol smaak gemaakt met dank aan de nauur.', '1.69', 'drankje'),
+(28, 'fernades cherry bouquet', 'fernadez_cherrybouquet.png', 'de heerlijke smaak cherry in een heerlijk drankje,fernandes maak er wat van.', '1.39', 'drankje'),
+(29, 'fernandez green punch', 'fernandez_greenpunch.png', 'green punch een heerlijk drankje,fernandes maak er wat van.', '1.39', 'drankje'),
+(30, 'fernandez super pineapple', 'fernandez_superpineapple.png', 'de heerlijke PINEAPPLE smaak in een SUPER heerlijk drankje,fernandes maak er wat van.', '1.39', 'drankje'),
+(31, 'kidz menu box 1', 'box1.png', 'box 1 kinder menu. dit menu bevat het volgende: 1 bakje popcorn zoet of zout,1 zakje skittles, een appelsap en een verassing.', '10.49', 'kidzMenu'),
+(32, 'kidz menu box 2', 'box2.png', 'box 2 kinder menu. dit menu bevat het volgende: 1 zakje lays chips naturel of paprika,1 zakje skittles, een appelsap en een leukere verassing.', '12.49', 'kidzMenu'),
+(33, 'kidz menu box 3', 'box3.png', 'box 3 kinder menu. dit menu is voor de kinderen die alleen de verassing willen.', '3.99', 'kidzMenu');
 
 --
 -- Indexen voor geëxporteerde tabellen
