3d269a0f3f7c82b9625ce755d3c73492313e8b18
Swen <99058148@mydavinci.nl>
2020-06-30 11:54:30 +0200

    Klantenservice pagina af

--- patwee/view/home/klanten.php
+++ patwee/view/home/klanten.php
@@ -6,16 +6,36 @@
 <div class="d-flex justify-content-around">
 
 <div class="text-center">
-<i class="fas fa-mobile-alt text-center"></i>
+<i class="fas fa-mobile-alt text-center">
+	<br>
+	<br>
+	Klantenservice: 088-51 52 000<br><br>
+	Swen: 06-40808457<br><br>
+	Anthony: 06-36470448
+</i>
 </div>
 
 <div class="text-center">
-<i class="fas fa-envelope"></i></i>
+<i class="fas fa-clock">
+	<br>
+	<br>
+	Open: 15 minuten voor de start van de eerste voorstelling<br><br>
+	Sluit: 10 Minuten na de laatste voorstelling<br>
+</i>
 </div>
 
 <div class="text-center">
-<i class="fas fa-mobile-alt text-center"></i>
+<i class="fas fa-envelope">
+	<br>
+	<br>
+	Klantenservice: events@pathe.nl<br><br>
+	Swen: swentjuuh@gmail.com<br><br>
+	Anthony: inocencio11@outlook.com
+</i>
 </div>
+
+
+<p class="fixed-bottom" style="margin-bottom:100px; margin-left:500px;">Voor meer informatie ga naar:&nbsp; <a href="https://www.pathe.nl/business/service-contact">https://www.pathe.nl/business/service-contact</a></p>
 
 </div>
 
