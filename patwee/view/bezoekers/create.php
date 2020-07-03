3bba12039dc73dc4a23ac726d6066232b1664722
anthony <99057187@mydavinci.nl>
2020-07-03 10:30:01 +0200

    create bug fixen

--- patwee/view/bezoekers/create.php
+++ patwee/view/bezoekers/create.php
@@ -1,6 +1,9 @@
 </nav>
 <?php
-
+	if(empty($nameErr || $leeftijdErr || $telNummerErr)){
+		$nameErr = $leeftijdErr = $telNummerErr = "";
+		$name = $leeftijd = $telNummer = "";
+	}
 ?>
 <body class="bg-warning">
 	<img src="<?php echo URL ?>public/afbeeldingen/logo/patwee_logo_black.png?>" style="width:150px; position: absolute; left:720px; top:115px;">
